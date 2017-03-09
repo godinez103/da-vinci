969af90df3aede34050d870ab46adf5a856b4052
Usuário <Usurio@Usurio-PC>
2017-03-09 14:29:02 -0300

    criada as classes main, manipular arquivo, retangulo.

---
+++ da vinci/src/Retangulo.java
@@ -0,0 +1,40 @@
+
+public class Retangulo {
+
+	private int x;
+	private int y;
+	private int tamanho;
+	private char cor;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public char getCor() {
+		return cor;
+	}
+
+	public void setCor(char cor) {
+		this.cor = cor;
+	}
+}
---
+++ da vinci/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	private int altura;
+	private String figuratxt;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
