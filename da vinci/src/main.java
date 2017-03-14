17d9a000119fc203d785f971694e5daa01492f77
Usuário <Usurio@Usurio-PC>
2017-03-14 10:14:36 -0300

    adicionados os arquivos txt

--- da vinci/src/main.java
+++ da vinci/src/main.java
@@ -1,12 +1,15 @@
+import java.io.IOException;
 
 public class main {
 
 	private int altura;
 	private String figuratxt;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		
+		ManipuladorArquivo.leitor("/da vinci/src/t2b.txt");
+		
 	}
 
 }
